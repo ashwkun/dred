9ecbcc2ec22189dcb2a950a52f82a0f5e5ffd01b
Aswin C <aswinanand9020@gmail.com>
2025-02-23 04:31:41 +0530

    deploy update

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyB-9yZNPFFsjG8JR5t9i6ZbYZ9FnbZegw8",
+  authDomain: "dred-e6e59.firebaseapp.com",
+  projectId: "dred-e6e59",
+  storageBucket: "dred-e6e59.firebasestorage.app",
+  messagingSenderId: "901054214672",
+  appId: "1:901054214672:web:d29954f1846e1b337d1095"
 };
 
 const app = initializeApp(firebaseConfig);
